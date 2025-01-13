e624440af1e660bf8dda3d2c4e8e884c41c5784d
Aaryan6 <aaryanpatel683@gmail.com>
2025-01-13 19:48:43 +0530

    changes in vercel.json

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
     return [
       {
         source: "/api/py/:path*",
-        destination: "http://127.0.0.1:8000/py/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/py/:path*"
+            : "/api/",
       },
     ];
   },
